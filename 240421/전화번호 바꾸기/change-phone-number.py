365b90ce1329917782f24e7760d83e35815c77b5
장은수 <38238197+Adler-1014@users.noreply.github.com>
2024-04-21 18:00:24 +0900

    Title: 전화번호 바꾸기, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240421/전화번호 바꾸기/change-phone-number.py
+++ 240421/전화번호 바꾸기/change-phone-number.py
@@ -2,5 +2,6 @@ inputs = input().split("-")
 if inputs[0] != "010":
     print("첫 번째 입력은 '010'이어야 합니다.")
 else:
-    a, b, c = map(int, inputs)
+    a = inputs[0]
+    b,c =inputs[1],inputs[2]
     print(f"{a}-{c}-{b}")
