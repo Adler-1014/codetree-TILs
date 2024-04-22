bffb022d55b24d2566c5c56ffd80d57b555dd86a
장은수 <38238197+Adler-1014@users.noreply.github.com>
2024-04-22 15:30:18 +0900

    Title: 두수의 곱과 몫, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240422/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.py
+++ 240422/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.py
@@ -2,5 +2,5 @@ a,b = map(int,input().split())
 
 ans_1 = int(a*b)
 asn_2 = int(a/b)
-print(f"{a} * {b} = {ans1}")
-print(f"{a} / {b} = {ans2}")
+print(f"{a} * {b} = {ans_1}")
+print(f"{a} / {b} = {ans_2}")
